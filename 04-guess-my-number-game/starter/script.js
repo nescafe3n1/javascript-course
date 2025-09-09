7a0ebc5a4332939a1f45f259c7ffafec8c5d3c77
nescafe <leueilshemnaranjo521@gmail.com>
2025-09-10 07:48:38 +0800

    feat: add Guess my Number Game start files and documentation

---
+++ 04-guess-my-number-game/starter/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log(`=== GAME DEVELOPEMENT: GUESS MY NUMBER ===`);
+
+console.log(`Goal: Build a complete interactive game from scratch`);
+console.log(`Focus: DOM manipulation, game state, and user interaction`);
+
